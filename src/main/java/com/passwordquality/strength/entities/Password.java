323d4bb32eb717dfd9f3d02b4a2619c9d5bceb60
Matheus R Linhaus <matheuslinhauss@hotmial.com>
2024-11-06 18:21:49 -0300

    Criado um end point onde pode ser chamado a API de senha.

---
+++ src/main/java/com/passwordquality/strength/entities/Password.java
@@ -0,0 +1,62 @@
+package com.passwordquality.strength.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "weak_password")
+public class Password {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String password;
+	
+	public Password() {
+		
+	}
+
+	public Password(Long id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Password other = (Password) obj;
+		return Objects.equals(password, other.password);
+	}
+}
