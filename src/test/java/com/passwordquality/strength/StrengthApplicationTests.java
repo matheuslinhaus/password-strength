51ef83d444fbbe67ea82fa30012999d70e613a70
Matheus R Linhaus <matheuslinhauss@hotmial.com>
2024-11-06 17:26:11 -0300

    First commit

---
+++ src/test/java/com/passwordquality/strength/StrengthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordquality.strength;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrengthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
